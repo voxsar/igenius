97974c5c2fbb3c2352a97b47e91827d9926d9463
voxsar <vo.x@outlook.com>
2018-02-14 17:58:45 +0530

    [skip ci] quiz-master

--- quiz-master/app/main.js
+++ quiz-master/app/main.js
@@ -1,9 +1,9 @@
 const electron = require('electron')
 // Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
 
+const {app, BrowserWindow} = require('electron');
+ 
+require('electron-reload')(__dirname);
 const path = require('path')
 const url = require('url')
 
@@ -13,8 +13,8 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
+  mainWindow = new BrowserWindow({maximize: true, width: 800, height: 600})
+  mainWindow.maximize()
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
